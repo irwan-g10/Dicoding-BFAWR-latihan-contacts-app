2b2cbcaec2380dfef96af89dec789fb1674a4a17
irwan-g10 <irwangumilar111@gmail.com>
2022-09-20 16:56:41 +0700

    React Context

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -7,6 +7,7 @@ import RegisterPage from '../pages/RegisterPage';
 import LoginPage from '../pages/LoginPage';
 
 import { getUserLogged, putAccessToken } from '../utils/api';
+import { LocaleProvider } from '../context/LocaleContext';
 
 class ContactApp extends React.Component {
 
@@ -15,7 +16,22 @@ class ContactApp extends React.Component {
 
         this.state = {
             authedUser: null,
-            initializing: true
+            initializing: true,
+            localeContext: {
+                locale: localStorage.getItem('locale') || 'id',
+                toggleLocale: () => {
+                    this.setState((prevState) => {
+                        const newLocale = prevState.localeContext.locale === 'id' ? 'en' : 'id'
+                        localStorage.setItem('locale', newLocale)
+                        return {
+                            localeContext: {
+                                ...prevState.localeContext,
+                                locale: newLocale
+                            }
+                        }
+                    })
+                }
+            }
         }
 
         this.onLoginSuccess = this.onLoginSuccess.bind(this);
@@ -33,7 +49,6 @@ class ContactApp extends React.Component {
         })
     }
     onLogout() {
-        // console.log('ss');
         this.setState(() => {
             return {
                 authedUser: null
@@ -57,32 +72,40 @@ class ContactApp extends React.Component {
         if (this.state.initializing) {
             return null;
         }
+
         if (this.state.authedUser === null) {
-            return (<div className='contact-app'>
-                <header className='contact-app__header'>
-                    <h1>Aplikasi Kontak</h1>
-                </header>
-                <main>
-                    <Routes>
-                        <Route path="/*" element={<LoginPage loginSuccess={this.onLoginSuccess} />} />
-                        <Route path="/register" element={<RegisterPage />} />
-                    </Routes>
-                </main>
-            </div>)
+            return (
+                <LocaleProvider value={this.state.localeContext}>
+                    <div className='contact-app'>
+                        <header className='contact-app__header'>
+                            <h1>Aplikasi Kontak</h1>
+                        </header>
+                        <main>
+                            <Routes>
+                                <Route path="/*" element={<LoginPage loginSuccess={this.onLoginSuccess} />} />
+                                <Route path="/register" element={<RegisterPage />} />
+                            </Routes>
+                        </main>
+                    </div>
+                </LocaleProvider>
+            )
         }
+
         return (
-            <div className="contact-app">
-                <header className='contact-app__header'>
-                    <h1>Aplikasi Kontak</h1>
-                    <Navigation logout={this.onLogout} name={this.state.authedUser.name} />
-                </header>
-                <main>
-                    <Routes>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/add" element={<AddPage />} />
-                    </Routes>
-                </main>
-            </div>
+            <LocaleProvider value={this.state.localeContext}>
+                <div className="contact-app">
+                    <header className='contact-app__header'>
+                        <h1>{this.state.localeContext.locale === 'id' ? 'Aplikasi Kontak' : 'Contacts App'}</h1>
+                        <Navigation logout={this.onLogout} name={this.state.authedUser.name} />
+                    </header>
+                    <main>
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/add" element={<AddPage />} />
+                        </Routes>
+                    </main>
+                </div>
+            </LocaleProvider>
         );
     }
 }
